3b3d53e61849ef6f402d3b8003069e0570967d3d
Astro <astro@spaceboyz.net>
2009-07-19 16:25:04 +0200

    LogicGates: add ConstGate

--- src/LogicGates.hs
+++ src/LogicGates.hs
@@ -2,16 +2,18 @@ module LogicGates (LogicGate(..)) where
 
 import IntegratedCircuit
 
-data LogicGate = NotGate
+data LogicGate = ConstGate Pin
+               | NotGate
                | AndGate
                | OrGate
                | XorGate
                  deriving (Show)
 
 instance IntegratedCircuitComponent LogicGate where
-	tick NotGate [i1] = (NotGate, [not i1])
-	tick AndGate [i1, i2] = (AndGate, [i1 && i2])
-	tick OrGate [i1, i2] = (OrGate, [i1 || i2])
-	tick XorGate [i1, i2] = (XorGate, [o1])
-	    where o1 = (i1 || i2) && not (i1 && i2)
+    tick gate@(ConstGate pin) [] = (gate, [pin])
+    tick NotGate [i1] = (NotGate, [not i1])
+    tick AndGate [i1, i2] = (AndGate, [i1 && i2])
+    tick OrGate [i1, i2] = (OrGate, [i1 || i2])
+    tick XorGate [i1, i2] = (XorGate, [o1])
+	where o1 = (i1 || i2) && not (i1 && i2)
   
